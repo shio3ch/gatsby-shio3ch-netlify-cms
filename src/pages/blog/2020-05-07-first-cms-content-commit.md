c22fbf5216f9ae72dcb7ecc1cf0c004add538413
shio3ch <rrrrse13@gmail.com>
2020-05-14 09:33:55 +0900

    init design

--- src/pages/blog/2020-05-07-first-cms-content-commit.md
+++ src/pages/blog/2020-05-07-first-cms-content-commit.md
@@ -4,7 +4,7 @@ title: First CMS content commit
 date: 2020-05-07T13:14:03.012Z
 description: 初めての投稿をしてみる。
 featuredpost: false
-featuredimage: /img/blog-index.jpg
+featuredimage: /img/og-image.jpg
 tags:
   - test
 ---
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -9,13 +9,8 @@ import BlogRoll from "../components/BlogRoll";
 import './template.sass'
 
 export const IndexPageTemplate = ({
-  image,
   title,
-  heading,
   subheading,
-  mainpitch,
-  description,
-  intro,
 }) => (
   <div>
     <section class="hero is-medium is-primary is-bold">
@@ -54,15 +49,8 @@ export const IndexPageTemplate = ({
 );
 
 IndexPageTemplate.propTypes = {
-  image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
   title: PropTypes.string,
-  heading: PropTypes.string,
   subheading: PropTypes.string,
-  mainpitch: PropTypes.object,
-  description: PropTypes.string,
-  intro: PropTypes.shape({
-    blurbs: PropTypes.array,
-  }),
 };
 
 const IndexPage = ({ data }) => {
@@ -71,13 +59,8 @@ const IndexPage = ({ data }) => {
   return (
     <Layout>
       <IndexPageTemplate
-        image={frontmatter.image}
         title={frontmatter.title}
-        heading={frontmatter.heading}
         subheading={frontmatter.subheading}
-        mainpitch={frontmatter.mainpitch}
-        description={frontmatter.description}
-        intro={frontmatter.intro}
       />
     </Layout>
   );
@@ -98,34 +81,7 @@ export const pageQuery = graphql`
     markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
       frontmatter {
         title
-        image {
-          childImageSharp {
-            fluid(maxWidth: 2048, quality: 100) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-        heading
         subheading
-        mainpitch {
-          title
-          description
-        }
-        description
-        intro {
-          blurbs {
-            image {
-              childImageSharp {
-                fluid(maxWidth: 240, quality: 64) {
-                  ...GatsbyImageSharpFluid
-                }
-              }
-            }
-            text
-          }
-          heading
-          description
-        }
       }
     }
   }
